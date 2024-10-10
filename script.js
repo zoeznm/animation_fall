d2134a23751e2674ff7b2ef4292b4369a2bb8412
zoeznm <bomikim046@gmail.com>
2024-10-10 10:58:17 +0900

    캐릭터가 걷고 넘어지는 애니메이션 실패

---
+++ script.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+  document.getElementById('paper1').style.animation = "drop 1s forwards";
+  document.getElementById('paper2').style.animation = "drop 1.2s forwards";
+  document.getElementById('paper3').style.animation = "drop 1.4s forwards";
+}, 1500);
